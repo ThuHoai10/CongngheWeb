401208b3c976af7cda7dc58683790eeb9798b188
Thu Hoài <hoaiarmy2005@gmail.com>
2025-11-29 22:39:33 +0700

    Delete TH1 directory

--- TH1/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli('localhost', 'root', '', 'th1_new');
-$conn->set_charset("utf8");
-
-if ($conn->connect_error) {
-    die("Kết nối thất bại: " . $conn->connect_error);
-}
-?>
